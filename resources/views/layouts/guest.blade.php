ca68fbc90d2252af05735947c0e1e543ec7a1e96
Arvid de Jong <arvid@darvis.nl>
2025-08-26 14:20:33 +0200

    Guest layout guest favicon added

--- resources/views/layouts/guest.blade.php
+++ resources/views/layouts/guest.blade.php
@@ -6,7 +6,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>{{ $title ?? 'CMS' }}</title>
     <meta name="author" content="Darvis | Arvid de Jong | info@arvid.nl">
-    <link rel="icon" type="image/png" href="/manta/img/favicon.png">
+    <link rel="icon" type="image/png" href="/vendor/manta-cms/manta/img/favicon.png">
     <meta name="csrf-token" content="{{ csrf_token() }}">
 
     <link rel="preconnect" href="https://fonts.bunny.net">
