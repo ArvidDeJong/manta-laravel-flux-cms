d32c2343bbae08047104428c69eae9bbb2fd4252
Arvid de Jong <arvid@darvis.nl>
2025-09-16 07:54:27 +0200

    WithSortingTrait improved

--- src/Traits/WithSortingTrait.php
+++ src/Traits/WithSortingTrait.php
@@ -6,22 +6,6 @@ use Livewire\Attributes\Url;
 
 trait WithSortingTrait
 {
-    // #[Url]
-    // public $sortCol;
-
-    // #[Url]
-    // public $sortAsc = false;
-
-    // public function sortBy($column)
-    // {
-    //     if ($this->sortCol === $column) {
-    //         $this->sortAsc = !$this->sortAsc;
-    //     } else {
-    //         $this->sortCol = $column;
-    //         $this->sortAsc = false;
-    //     }
-    // }
-
     protected function applySorting($query)
     {
         if ($this->sortBy) {
